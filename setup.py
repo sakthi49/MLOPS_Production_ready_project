1ab42900a1e32d540cbcb4133c92514802fdae13
sakthi49 <sakthikumaran.1997@gmail.com>
2024-07-18 09:04:24 +0530

    third  commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="sakthi49",
+    author_email="sakthikumaran.1997@gmail.com",
+    packages=find_packages()
+)
